1707f9f04c3b82363eeba577a7380bae984cbcd8
Karine CHAMPEIL <kchampeilpro@gmail.com>
2020-11-25 17:07:20 +0100

    First cleaning : exception management, deletion of unuseful variables, closure of reader at the end of treatment

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,57 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FileNotFoundException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int dialatedpupilCount = 0;
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+	private static final String SYMPTOMSFILENAME = "C:\\tests\\P2\\symptoms.txt";
+	private static final String RESULTSFILENAME = "C:\\tests\\P2\\results.out";
+
+	public static void main(String[] args) throws Exception {
+		try {
+			// first get input
+
+			BufferedReader reader = new BufferedReader (new FileReader(SYMPTOMSFILENAME));
+			String line = reader.readLine();
+
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				switch (line) {
+					case "headache":
+						headacheCount++;
+						break;
+					case "rash":
+						rashCount++;
+						break;
+					case "dialated pupils":
+						dialatedpupilCount++;
+						break;
+				}
+
+				line = reader.readLine();	// get another symptom
 			}
 
-			line = reader.readLine();	// get another symptom
+			reader.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("input file or folder for input file does not exist");
+			return;
 		}
+
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		try {
+			FileWriter writer = new FileWriter(RESULTSFILENAME);
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + dialatedpupilCount + "\n");
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("folder for output file not available");
+		}
 	}
 }
